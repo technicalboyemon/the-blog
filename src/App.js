9cda4850f934c1bca5128bfed08aaaa4f0b33629
Emon Ahmed <technicalboyemon@gmail.com>
2021-12-29 19:11:50 +0600

    All Files uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Emon } from './Components/Emon';
 
 function App() {
+  const e = "Developer";
+  var ea = "Full stack"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h">
+        Emon Ahmed {ea}
+      </div>
+      <Emon />
+    </>
   );
 }
 
---
+++ src/Components/Emon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Emon = () => {
+    return (
+        <div>
+            Hi Emon
+        </div>
+    )
+}
